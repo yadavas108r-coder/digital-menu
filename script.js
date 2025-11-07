ecdc44667f4691aa85003af81b19d9b0da976226
yadavas108r-coder <yadavas108r@gmail.com>
2025-11-07 10:04:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const SHEET_URL = "https://script.google.com/macros/s/AKfycby6obWhcDkAuTC4IeS0ezRDRO3jPLVqUpKZDF5RpiaRZFMS2C2dcT3P-nZZ9bnjj_pe/exec";
+const SHEET_URL = "https://script.google.com/macros/s/AKfycbzllOLTfptlYXLFPE-h1wERyyusr1XCX0uWpe-szkWM9zBoA-VMQYqGLpCx7jKpO8S3/exec";
 const MENU_CACHE_KEY = "gd_menu_cache_v1";
 const CART_CACHE_KEY = "gd_cart_cache_v1";
 const MENU_CACHE_TS = "gd_menu_cache_ts";
@@ -455,6 +455,7 @@ document.addEventListener("DOMContentLoaded", () => {
   
   loadMenu().catch(err => console.error("Menu load failed", err));
 });
+
 
 
 
